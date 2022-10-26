415dda97adbb0344338daa6d982d003f39a487c5
Yuri Ruas <ysdor.dev@gmail.com>
2022-10-26 01:49:13 -0300

    add styled components and create default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>Hello world</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  background: '#fafafa',
+
+  'base-button': '#e6e5e5',
+  'base-card': '#f3f2f2',
+  'base-hover': '#d7d5d5',
+  'base-input': '#ededed',
+  'base-label': '#8d8686',
+  'base-subtitle': '#403937',
+  'base-text': '#574f4d',
+  'base-title': '#272221',
+
+  yellow: '#dbac2c',
+  'yellow-dark': '#c47f17',
+  'yellow-light': '#f1e9c9',
+
+  purple: '#8047f8',
+  'purple-dark': '#4b2995',
+  'purple-light': '#ebe5f9',
+
+  white: '#fff',
+}
