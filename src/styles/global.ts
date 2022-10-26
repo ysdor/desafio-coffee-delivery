eda06a72dfcdabf569c8720ae1c54d9cf8a1a182
Yuri Ruas <ysdor.dev@gmail.com>
2022-10-26 01:58:50 -0300

    create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.background};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
